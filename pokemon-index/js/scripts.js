8dd44a90d46099e42e47b43e995100726bb0dbb1
lac1990 <155014030+lac1990@users.noreply.github.com>
2024-01-10 11:29:13 -0600

    Add files via upload

---
+++ pokemon-index/js/scripts.js
@@ -0,0 +1,31 @@
+let pokemonList = [
+    
+    {
+        name: 'eevee',
+        height: '.3m',
+        type: ['normal'],
+        abilities: ['runaway, adaptability, anticipation']
+    },
+    
+    {
+        name: 'jolteon',
+        height: '.8m',
+        type: 'electric',
+        abilities: ['volt absorb, quick-feet']
+    },
+
+    {
+        name: 'flareon',
+        height: '2 11',
+        type: 'fire',
+        abilities: ['flash-fire, guts']
+    },
+
+    {
+        name: 'vaporeon',
+        height: '1m',
+        type: 'water',
+        abilities: ['water absorb, hydration']
+    }
+
+]
